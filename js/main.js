34fab82f5acd836a3a6c888975778a2cbefad4c2
Aliosha <aliosha.shalabay@gmai.com>
2019-06-11 10:49:59 +0300

    index adaptive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 $("input[type='number']").inputSpinner();
+jQuery(document).ready(function () {
+    if ($(document).width() <  768)
+    $(".slider").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }],
+    });
+});
